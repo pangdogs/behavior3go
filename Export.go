7daa2bdb3bd81cb46c08d9bf7b6f5a04209547fa
pangdogs <pangdogs@gmail.com>
2021-10-24 19:40:29 +0800

    修改代码

--- Export.go
+++ Export.go
@@ -1,8 +1,11 @@
 package behavior3go
 
 import (
+	. "github.com/pangdogs/behavior3go/internal/actions"
+	. "github.com/pangdogs/behavior3go/internal/composites"
 	"github.com/pangdogs/behavior3go/internal/config"
 	"github.com/pangdogs/behavior3go/internal/core"
+	. "github.com/pangdogs/behavior3go/internal/decorators"
 	"github.com/pangdogs/behavior3go/internal/nodelib"
 )
 
@@ -11,6 +14,34 @@ type BehaviorTree = core.BehaviorTree
 var NewBevTree = core.NewBevTree
 
 type NodeLib = nodelib.NodeLib
+
+func NewNodeLib() *NodeLib {
+	nodeLib := nodelib.NewNodeLib()
+
+	// 默认actions
+	nodeLib.Register("Error", &Error{})
+	nodeLib.Register("Failer", &Failer{})
+	nodeLib.Register("Runner", &Runner{})
+	nodeLib.Register("Succeeder", &Succeeder{})
+	nodeLib.Register("Wait", &Wait{})
+	nodeLib.Register("Log", &Log{})
+
+	// 默认composites
+	nodeLib.Register("MemPriority", &MemPriority{})
+	nodeLib.Register("MemSequence", &MemSequence{})
+	nodeLib.Register("Priority", &Priority{})
+	nodeLib.Register("Sequence", &Sequence{})
+
+	// 默认decorators
+	nodeLib.Register("Inverter", &Inverter{})
+	nodeLib.Register("Limiter", &Limiter{})
+	nodeLib.Register("MaxTime", &MaxTime{})
+	nodeLib.Register("Repeater", &Repeater{})
+	nodeLib.Register("RepeatUntilFailure", &RepeatUntilFailure{})
+	nodeLib.Register("RepeatUntilSuccess", &RepeatUntilSuccess{})
+
+	return nodeLib
+}
 
 type Action = core.Action
 
--- internal/nodelib/NodeLib.go
+++ internal/nodelib/NodeLib.go
@@ -2,9 +2,6 @@ package nodelib
 
 import (
 	"errors"
-	. "github.com/pangdogs/behavior3go/internal/actions"
-	. "github.com/pangdogs/behavior3go/internal/composites"
-	. "github.com/pangdogs/behavior3go/internal/decorators"
 	"reflect"
 )
 
@@ -18,29 +15,6 @@ func NewNodeLib() *NodeLib {
 	nodeLib := &NodeLib{
 		nodeMap: make(map[string]reflect.Type),
 	}
-
-	// 默认actions
-	nodeLib.Register("Error", &Error{})
-	nodeLib.Register("Failer", &Failer{})
-	nodeLib.Register("Runner", &Runner{})
-	nodeLib.Register("Succeeder", &Succeeder{})
-	nodeLib.Register("Wait", &Wait{})
-	nodeLib.Register("Log", &Log{})
-
-	// 默认composites
-	nodeLib.Register("MemPriority", &MemPriority{})
-	nodeLib.Register("MemSequence", &MemSequence{})
-	nodeLib.Register("Priority", &Priority{})
-	nodeLib.Register("Sequence", &Sequence{})
-
-	// 默认decorators
-	nodeLib.Register("Inverter", &Inverter{})
-	nodeLib.Register("Limiter", &Limiter{})
-	nodeLib.Register("MaxTime", &MaxTime{})
-	nodeLib.Register("Repeater", &Repeater{})
-	nodeLib.Register("RepeatUntilFailure", &RepeatUntilFailure{})
-	nodeLib.Register("RepeatUntilSuccess", &RepeatUntilSuccess{})
-
 	return nodeLib
 }
 
